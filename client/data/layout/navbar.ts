568260f7bc760ecfb7f10066216247885942ace1
redsaucce <drewbenettw@gmail.com>
2026-07-28 13:46:23 +0800

    Update navbar links

--- client/data/layout/navbar.ts
+++ client/data/layout/navbar.ts
@@ -5,7 +5,6 @@ export const navbar = {
     { label: "Home", to: "/", section: null },
     { label: "Features", to: "/", section: "features" },
     { label: "How It Works", to: "/", section: "how-it-works" },
-    { label: "Pricing", to: "/", section: "pricing" },
     { label: "FAQs", to: "/", section: "faqs" },
     { label: "About Us", to: "/about", section: null },
   ],
